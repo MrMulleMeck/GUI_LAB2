bbc9ea7c83782c4efca1549097538ec320044ae9
MrMulleMeck <noel.elm04@hotmail.se>
2025-04-09 18:28:48 +0200

    Uppdelning av sidan, dropdown-menyer och kontroller osv 2

---
+++ lib/widgets/kitchen_control.dart
@@ -0,0 +1,27 @@
+import 'package:lab2/util/cuisine.dart';
+import 'package:flutter/material.dart';
+
+class CuisineControl extends StatelessWidget {
+  const CuisineControl({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    const labels = Cuisine.labels;
+
+    return DropdownMenu<String>(
+      width: 164,
+      enableFilter: false,      // Disables filtering (prevents typing)
+      requestFocusOnTap: false, // Prevents keyboard from opening
+      initialSelection: labels[0],  // Starts with the first element selected
+      dropdownMenuEntries: [
+        for (int i = 0; i < labels.length; i++)
+           DropdownMenuEntry(
+           value: labels[i],
+           label: labels[i],
+        ),
+      ],
+      onSelected: (value){},
+    );
+  }
+}
